642610e3771557a9914dcfb0108875a1cbaa7dc9
Abhishek Rathore <abhirathore565@gmail.com>
2023-03-10 23:16:30 +0530

    Integrated the modules and created few tests testing the basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,46 +2,26 @@ mod core;
 
 #[cfg(test)]
 mod tests {
-    use crate::core::types;
+    use crate::core::{types::{Key, Value}, store::Store, errors};
 
     #[test]
-    fn it_works() {
-        let key = types::Key::String("name".to_string());
-        let value1 = types::Value::String("abhishek".to_string());
-        let value2 = types::Value::Number(-1);
-        let value3 = types::Value::Float(2.5);
+    fn initialize_an_empty_store() {
+        let store = Store::new();
+        
+        assert_eq!(store.is_empty(), true);
+        assert_eq!(store.size(), 0);
+        assert_eq!(store.get_keys(), Vec::<&Key>::new());
+    }
 
-        match key {
-            types::Key::String(x) => {
-                assert!(x == "name");
-            }
-        }
-
-        match value1 {
-            types::Value::String(x) => {
-                assert!(x == "abhishek");
-            }
-            _ => {
-                assert!(false);
-            }
-        }
-
-        match value2 {
-            types::Value::Number(x) => {
-                assert!(x == -1);
-            }
-            _ => {
-                assert!(false);
-            }
-        }
-
-        match value3 {
-            types::Value::Float(x) => {
-                assert!(x == 2.5);
-            }
-            _ => {
-                assert!(false);
-            }
+    #[test]
+    fn writing_a_value_and_reading_it_back() {
+        let mut store = Store::new();
+    
+        store.set(Key::String("name".to_string()), Value::String("kelue".to_string()));    
+        
+        match store.get(&Key::String("name".to_string())) {
+            Ok(value) => assert_eq!(value, &Value::String("kelue".to_string())),
+            Err(errors::KelueError::KeyNotFoundError) => panic!("This should not happen as 'name' is present in the store"),
         }
     }
 }
