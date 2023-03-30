b9c66a5134c9a6f600b4e2cc40cb5294e99b7c6b
Abhishek Rathore <abhirathore565@gmail.com>
2023-03-30 21:04:47 +0530

    Added tests to check the functionality of the delete function

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,42 @@ mod tests {
     #[test]
     fn writing_a_value_and_reading_it_back() {
         let mut store = Store::new();
+        let name = String::from("name");
+        let kelue = String::from("kelue");
     
-        store.set(Key::String("name".to_string()), Value::String("kelue".to_string()));    
+        store.set(Key::String(name.clone()), Value::String(kelue.clone()));    
         
-        match store.get(&Key::String("name".to_string())) {
-            Ok(value) => assert_eq!(value, &Value::String("kelue".to_string())),
+        match store.get(&Key::String(name)) {
+            Ok(value) => assert_eq!(value, &Value::String(kelue)),
             Err(errors::KelueError::KeyNotFoundError) => panic!("This should not happen as 'name' is present in the store"),
+        }
+    }
+
+    #[test]
+    fn deleting_a_key_and_trying_to_read_it_back() {
+        let mut store = Store::new();
+        let name = String::from("name");
+        let kelue = String::from("kelue");
+
+        store.set(Key::String(name.clone()), Value::String(kelue));
+        store.delete(&Key::String(name.clone()));
+
+        match store.get(&Key::String(name)) {
+            Ok(value) => panic!("This key should not be present in the store, value: {:?}", value),
+            Err(errors::KelueError::KeyNotFoundError) => assert!(true),
+        }
+    }
+
+    #[test]
+    fn deleting_a_key_which_does_not_exist() {
+        let mut store = Store::new();
+        let name = String::from("name");
+
+        store.delete(&Key::String(name.clone()));
+
+        match store.get(&Key::String(name)) {
+            Ok(value) => panic!("This key should not be present in the store, value: {:?}", value),
+            Err(errors::KelueError::KeyNotFoundError) => assert!(true),
         }
     }
 }
